fb8481ac20200477ac2e33891187ccfd6dffd5ba
David Doan <ddoan17@mit.edu>
2015-07-04 10:44:27 -0700

    Optimized loading videos for modal only when open

--- js/agency.js
+++ js/agency.js
@@ -25,28 +25,53 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 })
 
+//Open video on open
+$('#portfolioModal1').on('show.bs.modal', function (e) {
+    $('#portfolioModal1 iframe').attr("src", "https://player.vimeo.com/video/98880732?color=ffffff");
+})
+
+$('#portfolioModal2').on('show.bs.modal', function (e) {
+    $('#portfolioModal2 iframe').attr("src", "https://www.youtube.com/embed/jX8hcrAGZtY");
+})
+
+$('#portfolioModal3').on('show.bs.modal', function (e) {
+    $('#portfolioModal3 iframe').attr("src", "https://www.youtube.com/embed/-0AhpfKFOEM?vq=hd720");
+})
+
+$('#portfolioModal4').on('show.bs.modal', function (e) {
+    $('#portfolioModal4 iframe').attr("src", "https://www.youtube.com/embed/KcwBB0xHAfU?vq=hd720");
+})
+
+$('#portfolioModal5').on('show.bs.modal', function (e) {
+    $('#portfolioModal5 iframe').attr("src", "");
+})
+
+$('#portfolioModal6').on('show.bs.modal', function (e) {
+    $('#portfolioModal6 iframe').attr("src", "");
+})
+
 //Close video on close
 
-$('#portfolioModal1').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal1 iframe').attr("src", $("#portfolioModal1 iframe").attr("src"));
+$('#portfolioModal1').on('hide.bs.modal', function (e) {
+	$('#portfolioModal1 iframe').attr("src", "");
 })
 
-$('#portfolioModal2').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal2 iframe').attr("src", $("#portfolioModal2 iframe").attr("src"));
+$('#portfolioModal2').on('hide.bs.modal', function (e) {
+	$('#portfolioModal2 iframe').attr("src", "");
 })
 
-$('#portfolioModal3').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal3 iframe').attr("src", $("#portfolioModal3 iframe").attr("src"));
+$('#portfolioModal3').on('hide.bs.modal', function (e) {
+	$('#portfolioModal3 iframe').attr("src", "");
 })
 
-$('#portfolioModal4').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal4 iframe').attr("src", $("#portfolioModal4 iframe").attr("src"));
+$('#portfolioModal4').on('hide.bs.modal', function (e) {
+	$('#portfolioModal4 iframe').attr("src", "");
 })
 
-$('#portfolioModal5').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal5 iframe').attr("src", $("#portfolioModal5 iframe").attr("src"));
+$('#portfolioModal5').on('hide.bs.modal', function (e) {
+	$('#portfolioModal5 iframe').attr("src", "");
 })
 
-$('#portfolioModal6').on('hidden.bs.modal', function (e) {
-	$('#portfolioModal6 iframe').attr("src", $("#portfolioModal6 iframe").attr("src"));
+$('#portfolioModal6').on('hide.bs.modal', function (e) {
+	$('#portfolioModal6 iframe').attr("src", "");
 })
